967568b0c4fa1909fe598f2c9d341b9963735662
Héctor López <hectorlopezl@outlook.com>
2022-05-23 06:45:33 -0500

    Add constants file

---
+++ constants.py
@@ -0,0 +1,9 @@
+edge_constants = {
+    "n_edges_range": (1, 3),
+    "weight_range": (1, 3),
+    "length_range": (1, 3),
+}
+node_constants = {
+    "size_range": (10, 20),
+    "endo_range": (1, 3),
+}
