a1bdcec1466c655ee820bb47bf0f5695e06da2e5
Héctor López <hectorlopezl@outlook.com>
2022-05-25 17:01:24 -0500

    Small refactor

--- edge.py
+++ edge.py
@@ -1,26 +1,22 @@
+from __future__ import annotations
 import json
 from nodes import Node
 
 
 class Edge:
     """"""
-    ALLOWED_ATTRIBUTES = {"weight", "length"}
     def __init__(
         self,
         node: Node,
         weight: int = None,
         length: int = None,
-        **kwargs
     ) -> None:
 
         self.node = node
         self.weight = weight
-        self.lenght = length
+        self.length = length
 
         self.next_steps: list = list()
-
-    def __hash__(self) -> int:
-        return hash(self.node.name)
 
     def dream_cycle(self) -> None:
         trickles_arriving = 1 in self.next_steps
@@ -29,19 +25,19 @@ class Edge:
         self.next_steps = [el - 1 for el in self.next_steps if el != 1]
 
     def load(self) -> None:
-        self.next_steps.append(self.lenght)
+        self.next_steps.append(self.length)
 
     def __hash__(self) -> int:
         return hash(self.node.name)
 
-    def __eq__(self, other_edge: object) -> bool:
+    def __eq__(self, other_edge: Edge) -> bool:
         return self.node.name == other_edge.node.name
 
     def __repr__(self) -> str:
         repr_ = {
             "to_node": self.node.name,
             "weight": self.weight,
-            "lenght": self.lenght,
+            "length": self.length,
             "next_steps": self.next_steps,
         }
         return json.dumps(repr_, indent=2)
