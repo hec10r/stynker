2ef08f8936ddb24a142034cf848b8630ca5aabb5
Héctor López <hectorlopezl@outlook.com>
2022-06-19 22:00:32 -0500

    Add distance_to_line and touch_border methods

--- src/environment.py
+++ src/environment.py
@@ -1,14 +1,14 @@
 import math
 import turtle
 from collections import deque
+from typing import List
+
 from .stynker import Stynker
 
 
 class Environment:
     def __init__(
         self,
-        stk1: Stynker,
-        stk2: Stynker = None,
         radius: int = 360,
         **kwargs
     ):
@@ -19,10 +19,6 @@ class Environment:
         self.window.setup(width, height)
         self.window.tracer(0)
 
-        # Initialize constants
-        self.gravity = -0.05
-        self.velocity = 0
-
         # Hexagon coordinates
         self.hexagon_coordinates = [
             (
@@ -31,6 +27,26 @@ class Environment:
             )
             for alpha in range(0, 420, 60)
         ]
+
+        rotated_coordinates = deque(self.hexagon_coordinates)
+        rotated_coordinates.rotate(-1)
+
+        # Lists to store parameters of general form equations
+        a_list = list()
+        b_list = list()
+        c_list = list()
+
+        for p1, p2 in zip(self.hexagon_coordinates[:6], list(rotated_coordinates)[:6]):
+            a = (p2[1] - p1[1]) / (p2[0] - p1[0])
+            b = -1
+            c = p1[1] - a * p1[0]
+            a_list.append(a)
+            b_list.append(b)
+            c_list.append(c)
+
+        # For each side of the hexagon, we are getting the
+        # a, b, c parameters who satisfy ax + by + c = 0
+        self.border_parameters = list(zip(a_list[:6], b_list[:6], c_list[:6]))
 
     def draw_borders(self):
         """Draw the borders of the environment"""
@@ -52,16 +68,17 @@ class Environment:
         self.window.update()
         return border
 
+    @staticmethod
+    def distance_to_line(x0, y0, a, b, c) -> float:
+        distance = abs((a * x0 + b * y0 + c)) / (math.sqrt(a * a + b * b))
+        return distance
+
     def touch_border(self, stk: turtle.Turtle) -> bool:
-        # TODO:
-        # Rotate coordinates
-        rotated_coordinates = deque(self.hexagon_coordinates)
-        rotated_coordinates.rotate(1)
-        print(rotated_coordinates)
-        for p1, p2 in zip(self.hexagon_coordinates, rotated_coordinates):
-            p0 = stk.pos()
-            # if height_triangle(p0, p1, p2) < 20:
-            #     return True
+        x0, y0 = stk.position()
+        for a, b, c in self.border_parameters:
+            distance = self.distance_to_line(x0, y0, a, b, c)
+            if distance < 10:
+                return True
         return False
 
     @staticmethod
