a34abcca8259f6216365a1fe8b81d2d8f205bcdd
Héctor López <hectorlopezl@outlook.com>
2022-06-27 18:30:38 -0500

    Add logic for running in multiple environments, and move here the moving responsibility

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
+import math
 import turtle
 from src import Stynker
 from src import Environment
 
 
 if __name__ == "__main__":
+    maze_environment = {
+        "border_coordinates": [
+            (360, -360),
+            (360, 360),
+            (-120, 360),
+            (-120, -180),
+            (-120, 360),
+            (-360, 360),
+            (-360, -360),
+            (120, -360),
+            (120, 180),
+            (120, -360),
+            (360, -360),
+        ],
+        "winning_segment": 5,
+        "losing_segment": 10,
+    }
+    hexagonal_radius = 360
+    hexagonal_environment = {
+        "border_coordinates": [
+            (
+                math.cos(math.radians(alpha)) * hexagonal_radius,
+                math.sin(math.radians(alpha)) * hexagonal_radius
+            )
+            for alpha in range(0, 360, 60)
+        ],
+        "winning_segment": 2,
+        "losing_segment": 5,
+    }
     # Initialize environment
-    environment = Environment()
+    environment = Environment(**hexagonal_environment)
     environment.draw_borders()
 
     # Input parameters
@@ -53,6 +83,10 @@ if __name__ == "__main__":
     stynker_1.period = "wake"
     stynker_2.period = "wake"
 
+    # Define vector magnitude for the Stynkers
+    stynker_1.vector_magnitude = 1.0
+    stynker_2.vector_magnitude = 1.0
+
     # Define two different initial velocity vectors
     stynker_1.velocity_vector = (stynker_1.vector_magnitude, 0)
     stynker_2.velocity_vector = (0, -stynker_2.vector_magnitude)
@@ -65,6 +99,10 @@ if __name__ == "__main__":
         # Check if the stynker bounces and calculate new velocity vector
         info_1 = environment.get_interaction_information(stynker_1)
         info_2 = environment.get_interaction_information(stynker_2)
+
+        # Update position
+        stynker_1.update_position(*info_1["new_position"])
+        stynker_2.update_position(*info_2["new_position"])
 
         # Update vector
         stynker_1.velocity_vector = info_1["final_velocity_vector"]
